e1327059e8df36db0c55c1b7e2a232d9b180d373
TheReaperGG <>
2021-05-31 15:51:31 +0000

    Aufgabe 1

--- zufall.cc
+++ zufall.cc
@@ -1,2 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double zufall(int a, int c, int m){
+  static int i = 16548432;
+  return i = ((a*i+c) % m);
+}
+
 int main() {
+  double na = 100;
+  double ni = 0;
+  int a = 5;
+  int c = 3;
+  int m = 16;
+
+  for(int i = 1; i <= na; i++){
+    double j = sqrt(pow(2*(zufall(a, c, m)/m)-1,2)+pow(2*(zufall(a ,c ,m)/m)-1, 2));
+    if(j < 1){
+      ni++;
+    }
+  }
+  double pi = ((ni * 4)/na);
+  double e = (M_PI/4)*na;
+  double v = (M_PI/4)*(1-(M_PI/4))*na;
+  double sig = 4*(sqrt(v)/na);
+
+  cout << "Pi: " << pi << endl;
+  cout << "Ni: " << ni << endl;
+  cout << "Erwartungswert: " << e << endl;
+  cout << "Varianz: " << v << endl;
+  cout << "Sigma pi: " << sig << endl;
 }
